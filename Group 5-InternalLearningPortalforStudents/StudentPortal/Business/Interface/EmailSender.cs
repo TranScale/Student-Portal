8ba2faaf7cfc7587c486256050b858e6dc7cc66f
hnp8uvnb <16102k412345@gmail.com>
2026-01-09 14:42:39 +0700

    +forgot password

---
+++ Group 5-InternalLearningPortalforStudents/StudentPortal/Business/Interface/EmailSender.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace StudentPortal.Business.Interface
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            // Thay vì gửi mail, ta in ra cửa sổ Output của Visual Studio
+            System.Diagnostics.Debug.WriteLine($"\n================ EMAIL START ================");
+            System.Diagnostics.Debug.WriteLine($"TO: {email}");
+            System.Diagnostics.Debug.WriteLine($"SUBJECT: {subject}");
+            System.Diagnostics.Debug.WriteLine($"CONTENT: {htmlMessage}");
+            System.Diagnostics.Debug.WriteLine($"================ EMAIL END ================\n");
+
+            return Task.CompletedTask;
+        }
+    }
+}
